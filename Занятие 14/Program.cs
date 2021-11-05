d961c13be5e0b199bbf0a45d2a7703dfacf8c024
Ирина Сумарокова <irina.sumarokova92@yandex.ru>
2021-11-05 16:12:10 +0300

    Добавьте файлы проекта.

---
+++ Занятие 14/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Занятие_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat(); 
+            cat.Name = "Кошка";
+            cat.ShowInfo();
+            cat.Say();
+
+            Dog dog = new Dog();
+            dog.Name = "Собака";
+            dog.ShowInfo();
+            dog.Say();
+
+            Console.ReadKey();
+        }
+    }
+    public abstract class Animal
+    {
+        public abstract string Name { get; set; }
+
+        public Animal()
+        {
+
+        }
+
+        public abstract void Say();
+        public void ShowInfo() 
+        {
+            Console.WriteLine("Животное - {0}",Name);
+        }
+    }
+   
+
+    public class Cat : Animal
+    { 
+        string name;
+
+        public Cat()
+            : base()
+        {
+            
+        }
+        public override void Say() 
+        {
+            Console.WriteLine("Голос - Мяу");
+        }
+        public override string Name 
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+    }
+
+    public class Dog:Animal
+    {
+        string name;
+
+        public Dog()
+            : base()
+        {
+           
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Голос - Гав");
+        }
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+    }
+
+}
